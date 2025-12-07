8f3285f1568e1bfea861c2402c03fb5ff6f74957
Alan Layng <alan.layng@gmail.com>
2025-12-07 12:36:48 +0000

    Initial version

---
+++ src/index.mjs
@@ -0,0 +1,42 @@
+//  Winkle
+//
+//  An object that will sleep for a time, but can be woken
+//
+export default class Winkle {
+  woken = false
+  #ms = null
+  #tm = null
+  #resolve = null
+
+  constructor (ms) {
+    if (ms != null) this.#ms = ms
+  }
+
+  get sleeping () {
+    return !!this.#tm
+  }
+
+  sleep (ms) {
+    ms = ms ?? this.#ms
+    if (ms == null) throw new Error('No time given')
+    if (this.sleeping) this.wake()
+
+    const { promise, resolve } = Promise.withResolvers()
+    this.#resolve = resolve
+    this.woken = false
+    this.#tm = setTimeout(this.#wake.bind(this, false), ms)
+    return promise
+  }
+
+  wake () {
+    this.#wake(true)
+  }
+
+  #wake (woken) {
+    if (!this.sleeping) return
+    clearTimeout(this.#tm)
+    this.woken = woken
+    this.#resolve(woken)
+    this.#tm = this.#resolve = null
+  }
+}
---
+++ test/index.mjs
@@ -0,0 +1,89 @@
+import { suite, test } from 'node:test'
+import assert from 'node:assert/strict'
+
+import Winkle from '../src/index.mjs'
+
+let tmStart
+function startClock () {
+  tmStart = Date.now()
+}
+
+function hasTaken (ms, tolerance = 4) {
+  return Math.abs(Date.now() - tmStart - ms) < tolerance
+}
+
+suite('Winkle', async () => {
+  test('basic construction', () => {
+    const w = new Winkle(20)
+    assert.ok(w instanceof Winkle)
+
+    assert.strictEqual(w.sleeping, false)
+    assert.strictEqual(w.woken, false)
+  })
+
+  test('sleep right through', async () => {
+    const w = new Winkle(20)
+
+    startClock()
+    const p = w.sleep()
+    assert.strictEqual(w.sleeping, true)
+
+    const bResult = await p
+    assert.ok(hasTaken(20))
+
+    assert.strictEqual(bResult, false)
+    assert.strictEqual(w.woken, false)
+    assert.strictEqual(w.sleeping, false)
+  })
+
+  test('sleep with wake, and non-default ms', async () => {
+    const w = new Winkle()
+
+    startClock()
+    const p = w.sleep(50)
+    assert.strictEqual(w.sleeping, true)
+
+    const w2 = new Winkle()
+    await w2.sleep(20)
+    w.wake()
+
+    const bResult = await p
+    assert.ok(hasTaken(20))
+
+    assert.strictEqual(bResult, true)
+    assert.strictEqual(w.woken, true)
+    assert.strictEqual(w.sleeping, false)
+  })
+
+  test('wake when not sleeping', async () => {
+    const w = new Winkle(20)
+    w.sleep()
+    w.wake()
+    assert.strictEqual(w.sleeping, false)
+    w.wake()
+    assert.strictEqual(w.sleeping, false)
+  })
+
+  test('sleep when already sleeping', async () => {
+    const w = new Winkle()
+    startClock()
+    const p1 = w.sleep(50)
+    const p2 = w.sleep(20)
+
+    const bResult1 = await p1
+    const bResult2 = await p2
+    assert.ok(hasTaken(20))
+
+    assert.strictEqual(bResult1, true)
+    assert.strictEqual(bResult2, false)
+  })
+
+  test('errors', async () => {
+    const w = new Winkle()
+    assert.throws(
+      //
+      () => w.sleep(),
+      /No time given/
+    )
+  })
+})
